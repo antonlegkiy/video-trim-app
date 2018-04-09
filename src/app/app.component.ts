823aec34002f0120207f99e4272f1d44ea175169
antonlegkiy <anton.legkiy@gmail.com>
2018-04-09 13:54:03 +0300

    req from front is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
+import 'rxjs/operators/map';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,13 @@ import {HttpClient, HttpParams} from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  duration: number;
   selectedFile = null;
   selectedFileName: string;
+  timing = {
+    from: { type: 'sec', value: null },
+    duration: { type: 'sec', value: null }
+  };
 
   constructor(private http: HttpClient) {}
 
@@ -17,7 +23,35 @@ export class AppComponent implements OnInit {
   onFileSelected(event) {
     this.selectedFile = event.target.files[0];
     this.selectedFileName = this.selectedFile.name;
+
+    const video = document.createElement('video');
+    video.preload = 'metadata';
+
+    video.onloadedmetadata = () => {
+      this.duration = video.duration;
+    };
+
+    const selectedVID = event.target.files[0];
+    video.src = URL.createObjectURL(selectedVID);
   }
 
-  onUploadFile() {}
+  onUploadFile() {
+    const formData: FormData = new FormData();
+    formData.append('file', this.selectedFile, this.selectedFile.name);
+
+    const params = new HttpParams()
+      .append('from', this.timing.from.type === 'min' ? this.timing.from.value * 60 : this.timing.from.value)
+      .append('duration', this.timing.duration.type === 'min' ? this.timing.duration.value * 60 : this.timing.duration.value);
+    const options = { params: params };
+
+    this.http.post(`/upload`, formData, options)
+      .subscribe(
+        data => {
+          console.log(data);
+        },
+        error => {
+          alert('failed to upload');
+        }
+      );
+  }
 }
